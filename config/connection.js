d8afe8eb4c3b3614e407c13fd8cf7f6f70845147
Michael Mount <mount.photos@gmail.com>
2023-03-19 13:55:09 -0600

    ran command npm i and tested connecting to server

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    //127.0.0.1 works well for those running on a macOS, if on windows, please use 'localhost'
+    dialect: "mysql",
+    host: "127.0.0.1",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
