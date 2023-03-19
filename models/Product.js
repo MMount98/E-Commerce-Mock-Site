347785371782b514621ee8c6e1c3bd4fddd096ad
Michael Mount <mount.photos@gmail.com>
2023-03-19 14:26:15 -0600

    built Product_tag model built refrence back to category id in products model

--- models/Product.js
+++ models/Product.js
@@ -28,8 +28,15 @@ Product.init(
       defaultValue: 10,
       validate: {
         isNumeric: true,
-      }
-    }
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
