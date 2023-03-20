d92fd76db921b38e30aec0b9c798348cdce23c88
Michael Mount <mount.photos@gmail.com>
2023-03-19 21:56:33 -0600

    built the routes for the categories

--- models/index.js
+++ models/index.js
@@ -1,16 +1,40 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const { belongsToMany } = require("./Category");
 
 // Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product);
+Product.belongsTo(Category);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
